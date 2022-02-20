884b656f6bc6092546cb3bd08a84c7708b1d2466
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2022-02-20 16:52:35 +0200

    Init react application. Add typescript, redux and prettier. Fix audit errors
    Folowing command were run
    npx create-react-app <name> --template redux-typescript
    npm install prettier --save-dev
    npm install eslint-config-prettier eslint-pluginprettier --save-dev
    npm audit fix --force
    
    Added .prettierrc file. Added settings to package.json related to prettier

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
