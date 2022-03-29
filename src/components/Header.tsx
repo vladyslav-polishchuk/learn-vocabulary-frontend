27ae85487b517a2179046800e6f00a7a29edc2ba
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2022-03-29 17:15:43 +0300

    Implement user menu in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,44 @@
-import { LibraryBooks } from '@mui/icons-material';
+import { LibraryBooks, AccountCircle } from '@mui/icons-material';
 
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Box,
+  Menu,
+  IconButton,
+  Tooltip,
+  MenuItem,
+} from '@mui/material';
+
+const settings = ['Profile', 'Logout'];
 
 export default function Header() {
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <AppBar position="fixed">
       <Container>
-        <Toolbar variant="dense">
+        <Toolbar variant="dense" sx={{ height: '56px' }}>
           <Typography
             variant="h6"
             color="inherit"
             component={Link}
             to="/"
-            sx={{ textDecoration: 'none' }}
+            sx={{ textDecoration: 'none', flexGrow: '1' }}
           >
             <LibraryBooks
               sx={{
@@ -25,6 +50,39 @@ export default function Header() {
             />
             Vocabulary
           </Typography>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton
+                size="large"
+                onClick={handleOpenUserMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
