c345632add52c41f5f07ab56d011722d748753fb
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2022-03-30 17:28:25 +0300

    Add pagination

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { FilterAlt, FilterAltOff } from '@mui/icons-material';
+import { FilterAlt, FilterAltOff, UploadFile } from '@mui/icons-material';
 
 import React from 'react';
 import {
@@ -13,6 +13,7 @@ import {
   Tooltip,
   Collapse,
   Badge,
+  Pagination,
 } from '@mui/material';
 import { ChangeEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -22,11 +23,14 @@ import Spinner from '../components/Spinner';
 import WordCard from '../components/WordCard';
 import WordDialog from '../components/WordDialog';
 
+const pageSize = 50;
+
 export default function Home() {
   const [filterOpened, setFilterOpened] = React.useState(false);
   const [minLength, setMinLength] = React.useState<string>('');
   const [maxLength, setMaxLength] = React.useState<string>('');
   const [search, setSearch] = React.useState<string>('');
+  const [page, setPage] = React.useState(1);
 
   const badgeCount =
     0 + (search ? 1 : 0) + (minLength ? 1 : 0) + (maxLength ? 1 : 0);
@@ -44,25 +48,68 @@ export default function Home() {
     dispatch(selectFile(file));
   };
 
-  const wordNodes = words
-    .filter((word) => {
-      const minLengthNum = parseInt(minLength ?? '') || 0;
-      const maxLengthNum = parseInt(maxLength ?? '') || 20;
-
-      return (
-        word.value.length >= minLengthNum &&
-        word.value.length <= maxLengthNum &&
-        word.value.includes(search)
-      );
-    })
-    .map((word) => (
-      <Grid item key={word.value} xs={4}>
-        <WordCard
-          word={word}
-          learnMoreHandler={() => dispatch(selectWord(word))}
-        ></WordCard>
+  if (!fileName) {
+    return (
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h3" align="center" sx={{ fontWeight: 'bold' }}>
+            Pick file to proceed
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              width: '100%',
+              height: '300px',
+              my: 2,
+              border: 'dashed',
+              borderColor: 'rgba(0, 0, 0, 0.12)',
+              borderRadius: '4px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              cursor: 'pointer',
+            }}
+            component="label"
+            htmlFor="contained-button-file"
+          >
+            <Input
+              id="contained-button-file"
+              type="file"
+              sx={{ display: 'none' }}
+              onChange={handleFiles}
+            />
+            <UploadFile sx={{ width: '72px', height: '72px' }} />
+            <Typography variant="h3" align="center">
+              Drop file or click
+            </Typography>
+          </Box>
+        </Grid>
       </Grid>
-    ));
+    );
+  }
+
+  const filteredWords = words.filter((word) => {
+    const minLengthNum = parseInt(minLength ?? '') || 0;
+    const maxLengthNum = parseInt(maxLength ?? '') || 20;
+
+    return (
+      word.value.length >= minLengthNum &&
+      word.value.length <= maxLengthNum &&
+      word.value.includes(search)
+    );
+  });
+
+  const numberOfPages = Math.ceil(filteredWords.length / pageSize);
+  const x = (page - 1) * pageSize;
+  const wordNodes = filteredWords.slice(x, x + pageSize).map((word) => (
+    <Grid item key={word.value} xs={4}>
+      <WordCard
+        word={word}
+        learnMoreHandler={() => dispatch(selectWord(word))}
+      ></WordCard>
+    </Grid>
+  ));
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -73,20 +120,8 @@ export default function Home() {
         onClose={() => dispatch(selectWord(null))}
       />
 
-      <Typography variant="h2" align="center" sx={{ fontWeight: 'bold' }}>
-        Home Page ({words.length})
-      </Typography>
-
-      <Toolbar variant="dense" disableGutters sx={{ my: 2 }}>
-        <TextField
-          variant="outlined"
-          disabled
-          value={fileName}
-          size="small"
-          sx={{ width: '50%' }}
-        ></TextField>
-
-        <Box sx={{ flexGrow: '1', mx: 1 }}>
+      <Box position="sticky">
+        <Toolbar variant="dense" disableGutters sx={{ my: 2 }}>
           <label htmlFor="contained-button-file">
             <Input
               id="contained-button-file"
@@ -98,65 +133,83 @@ export default function Home() {
               Pick File
             </Button>
           </label>
-        </Box>
 
-        <Tooltip title="Add Filters">
-          <IconButton
+          <Box sx={{ flexGrow: '1', mx: 1 }}>
+            <Tooltip title="Add Filters">
+              <IconButton
+                color="primary"
+                component="span"
+                onClick={() => setFilterOpened(!filterOpened)}
+              >
+                <Badge badgeContent={badgeCount} color="primary">
+                  <FilterAlt />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+          </Box>
+
+          <Pagination
+            page={page}
+            count={numberOfPages}
             color="primary"
-            component="span"
-            onClick={() => setFilterOpened(!filterOpened)}
-          >
-            <Badge badgeContent={badgeCount} color="primary">
-              <FilterAlt />
-            </Badge>
-          </IconButton>
-        </Tooltip>
-      </Toolbar>
+            onChange={(e, newPage) => setPage(newPage)}
+          />
+        </Toolbar>
 
-      <Collapse in={filterOpened} sx={{ mb: 2 }}>
-        <Toolbar variant="dense" disableGutters>
-          <TextField
-            variant="outlined"
-            size="small"
-            label="Minimal word length"
-            value={minLength}
-            onChange={(e) => setMinLength(e?.target?.value)}
-          ></TextField>
-
-          <Box sx={{ flexGrow: '1', marginLeft: 1 }}>
+        <Collapse in={filterOpened} sx={{ mb: 2 }}>
+          <Toolbar variant="dense" disableGutters>
             <TextField
               variant="outlined"
               size="small"
-              label="Maximal word length"
-              value={maxLength}
-              onChange={(e) => setMaxLength(e?.target?.value)}
-            ></TextField>
-          </Box>
-
-          <TextField
-            sx={{ mr: 1 }}
-            variant="outlined"
-            size="small"
-            label="Find"
-            value={search}
-            onChange={(e) => setSearch(e?.target?.value)}
-          ></TextField>
-
-          <Tooltip title="Clear Filters">
-            <IconButton
-              color="primary"
-              component="span"
-              onClick={() => {
-                setMinLength('');
-                setMaxLength('');
-                setSearch('');
+              label="Minimal word length"
+              value={minLength}
+              onChange={(e) => {
+                setMinLength(e?.target?.value);
+                setPage(1);
               }}
-            >
-              <FilterAltOff />
-            </IconButton>
-          </Tooltip>
-        </Toolbar>
-      </Collapse>
+            ></TextField>
+
+            <Box sx={{ flexGrow: '1', marginLeft: 1 }}>
+              <TextField
+                variant="outlined"
+                size="small"
+                label="Maximal word length"
+                value={maxLength}
+                onChange={(e) => {
+                  setMaxLength(e?.target?.value);
+                  setPage(1);
+                }}
+              ></TextField>
+            </Box>
+
+            <TextField
+              sx={{ mr: 1 }}
+              variant="outlined"
+              size="small"
+              label="Find"
+              value={search}
+              onChange={(e) => {
+                setSearch(e?.target?.value);
+                setPage(1);
+              }}
+            ></TextField>
+
+            <Tooltip title="Clear Filters">
+              <IconButton
+                color="primary"
+                component="span"
+                onClick={() => {
+                  setMinLength('');
+                  setMaxLength('');
+                  setSearch('');
+                }}
+              >
+                <FilterAltOff />
+              </IconButton>
+            </Tooltip>
+          </Toolbar>
+        </Collapse>
+      </Box>
 
       <Grid container spacing={2} sx={{ mb: 2 }}>
         <Grid item container xs={12} spacing={2}>
