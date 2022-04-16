0868b66246b6ef5e689779ad62ea352192190148
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2022-04-16 19:25:24 +0300

    Remove demo files from create-react-app template

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import vocabularyReducer from './vocabularySlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     vocabulary: vocabularyReducer,
   },
 });
